9c7cc86ae3bf6d97f6e499239e76d210e8bf5fc0
Gabriel Barbosa <gabrielbarbosaoliveira11999@gmail.com>
2022-11-23 23:37:52 -0300

    Backend project created

---
+++ backend/src/main/java/dev/gabrielbarbosa/gbcatalog/GbcatalogApplication.java
@@ -0,0 +1,13 @@
+package dev.gabrielbarbosa.gbcatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GbcatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GbcatalogApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/dev/gabrielbarbosa/gbcatalog/GbcatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.gabrielbarbosa.gbcatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GbcatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
